360327dd792d7a7e08010aa03b88bd91fd4e9b14
Daniil Martynov <DaniilDeFacto@yandex.ru>
2023-11-18 14:20:21 +0400

    Fix App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,7 +6,9 @@ import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.stream.Collectors;
 
