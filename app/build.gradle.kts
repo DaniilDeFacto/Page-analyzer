311483e90ff5cd6c9f70ce1bbe6e3554fa9818bd
Daniil Martynov <DaniilDeFacto@yandex.ru>
2023-11-20 13:44:36 +0400

    Add main.jte

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.postgresql:postgresql:42.1.4")
+    implementation("gg.jte:jte:3.0.1")
+    implementation("org.postgresql:postgresql:42.5.4")
     implementation("com.h2database:h2:2.2.220")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.slf4j:slf4j-simple:2.0.9")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,8 +2,12 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
@@ -16,26 +20,30 @@ import java.util.stream.Collectors;
 public class App {
 
     public static Javalin getApp() throws SQLException {
+
         var jdbcUrl = System.getenv("JDBC_DATABASE_URL");
         if (jdbcUrl == null) {
             jdbcUrl = "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;";
         }
         var hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(jdbcUrl);
-
         var dataSource = new HikariDataSource(hikariConfig);
-        var sql = getResourceFileAsString("schema.sql");
 
+        var sql = getResourceFileAsString("schema.sql");
         log.info(sql);
+
         try (var connection = dataSource.getConnection();
              var statement = connection.createStatement()) {
             statement.execute(sql);
         }
+
         BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> config.plugins.enableDevLogging());
 
-        app.get("/", ctx -> ctx.result("Hello World"));
+        JavalinJte.init(createTemplateEngine());
+
+        app.get("/", ctx -> ctx.render("main.jte"));
 
         return app;
     }
@@ -58,6 +66,13 @@ public class App {
     public static InputStream getResourceFileAsInputStream(String fileName) {
         ClassLoader classLoader = App.class.getClassLoader();
         return classLoader.getResourceAsStream(fileName);
+    }
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
     }
 
     public static void main(String[] args) throws SQLException {
